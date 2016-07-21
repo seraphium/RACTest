4f2b709d0185b378a265799e64d648b04b095ffa
张泽 <gjjisgod@gmail.com>
2016-07-21 17:01:23 +0800

    added  new duplex binding case added rac_command  case

--- RACTest/ViewController.swift
+++ RACTest/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 import ReactiveCocoa
 import Result
 
+struct RAC  {
+    var target : NSObject!
+    var keyPath : String!
+    var nilValue : AnyObject!
+    
+    init(_ target: NSObject!, _ keyPath: String, nilValue: AnyObject? = nil) {
+        self.target = target
+        self.keyPath = keyPath
+        self.nilValue = nilValue
+    }
+    
+    func assignSignal(signal : RACSignal) {
+        signal.setKeyPath(self.keyPath, onObject: self.target, nilValue: self.nilValue)
+    }
+}
+
+func RACObserve(target: NSObject!, keyPath: String) -> RACSignal  {
+    return target.rac_valuesForKeyPath(keyPath, observer: target)
+}
+
+infix operator ~> {}
+func ~> (signal: RACSignal, rac: RAC) {
+    rac.assignSignal(signal)
+}
+
+
 class ViewController: UIViewController {
 
     @IBOutlet var textField: UITextField!
     
-    var textValue : String! {
+    @IBOutlet weak var button: UIButton!
+    
+    dynamic var textValue : String! {
         didSet {
             print ("\(textValue)")
         }
@@ -25,7 +53,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         textValue = "test"
-        
+   
      /*   let textSignal = textField.rac_textSignal()
         textSignal.map { text -> AnyObject! in
             return text.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
@@ -51,15 +79,28 @@ class ViewController: UIViewController {
         textValue.producer.startWithNext { text in
             print ("textvalue put \(text)")
         }
+         //one type of two-way binding
         */
-        let textFieldChannel = textField.rac_newTextChannel()
+  /*     let textFieldChannel = textField.rac_newTextChannel()
         
         let textValueChannel =
             RACKVOChannel(target: self, keyPath: "textValue", nilValue: "")["followingTerminal"]
         textFieldChannel.subscribe(textValueChannel)
         textValueChannel.subscribe(textFieldChannel)
-        
-    
+ */
+        //another type of two-way binding
+        textField.rac_textSignal() ~> RAC(self, "textValue", nilValue: "")
+        RACObserve(self, keyPath: "textValue").skip(1).subscribeNext { text in
+            self.textField.text = text as? String
+        }
+
+        textValue = "he4llo"
+
+        button.rac_command = RACCommand { object -> RACSignal! in
+            self.textValue = "changed"
+            return RACSignal.empty()
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
