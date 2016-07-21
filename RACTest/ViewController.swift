90c0472111effe5c9f5d0ac9ca4997a020dd08ad
张泽 <gjjisgod@gmail.com>
2016-07-21 11:50:34 +0800

    added cases

--- RACTest/ViewController.swift
+++ RACTest/ViewController.swift
@@ -8,24 +8,47 @@
 
 import UIKit
 import ReactiveCocoa
+import Result
 
 class ViewController: UIViewController {
 
     @IBOutlet var textField: UITextField!
     
+    var textValue : MutableProperty<String>!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textValue = MutableProperty<String>("text")
         
-        
-        textField.rac_textSignal().map { text -> AnyObject! in
+        let textSignal = textField.rac_textSignal()
+        textSignal.map { text -> AnyObject! in
             return text.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
         }.filter { length -> Bool in
              length as! Int > 3
         }.subscribeNext { text in
             print (text)
         }
+        
+        let producer = timer(5, onScheduler: QueueScheduler.mainQueueScheduler).take(3)
+        producer.startWithSignal { signal, disposable in
+            signal.observe { date in
+                let d = date.value
+                print("\(d)")
+            }
+            
+        }
+      
+        textValue <~ textSignal.toSignalProducer().map { ($0 as? String) ?? ""}.flatMapError {
+            _ in SignalProducer<String, NoError>.empty
+        }
+        
+        textValue.producer.startWithNext { text in
+            print ("textvalue put \(text)")
+        }
+
+    
     }
 
     override func didReceiveMemoryWarning() {
